82e2e2d7fef6902530df2cc0bc7a4e9e3b1959af
tianxiongwu <tianxiongwu@tencent.com>
2022-01-23 12:08:10 +0800

    hello

--- main.go
+++ main.go
@@ -4,14 +4,25 @@
  */
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	//"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, world")
 }
